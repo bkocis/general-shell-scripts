ce0fb233181eb83c1e4ddff04f70b3ce444b6ea1
mnost <balaz.kocis@gmail.com>
2016-01-18 10:54:36 +0100

    1

--- term-pwd-log/open.sh
+++ term-pwd-log/open.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gnome-open $1
-
+gnome-open "$1"
+# the " " is needed for the case of whitespaces in a folder of filename 
 #var=`pwd`
 date=`date`
 echo "$date"" : ""$1" >> /home/mnost/.term_log2
